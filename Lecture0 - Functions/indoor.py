dfdbf9ccbf344c5e5d0eb3742922080b60abe40c
Pedro Luis <pedrolalves2006@gmail.com>
2024-01-04 18:57:25 -0300

    Lecture 0

---
+++ Lecture0 - Functions/indoor.py
@@ -0,0 +1,18 @@
+"""
+Indoor Voice
+
+WRITING IN ALL CAPS IS LIKE YELLING.
+
+Best to use your “indoor voice” sometimes, writing entirely in lowercase.
+
+In a file called indoor.py, implement a program in Python that prompts the 
+user for input and then outputs that same input in lowercase. 
+Punctuation and whitespace should be outputted unchanged.
+
+You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input.
+"""
+#----------------------------------------
+
+sentence = input(print("Please, let me show you how to use your \"indoor voice\". Write a sentence using CAPSLOCK \n")).lower()
+
+print(sentence)
